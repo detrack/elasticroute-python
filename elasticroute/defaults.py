4973dfc888c911dfea89c885cc487c212aafc867
chesnutcase <chester8991@gmail.com>
2019-06-19 16:30:54 +0800

    remove extra fields in default generalsettings

--- elasticroute/defaults.py
+++ elasticroute/defaults.py
@@ -3,9 +3,6 @@ API_KEY = None
 generalSettings = {
     'country': 'SG',
     'timezone': 'Asia/Singapore',
-    'map': 'OpenStreetMap',
-    'map_api_key': None,
-    'route_osm': 0,
     'loading_time': 20,
     'buffer': 40,
     'service_time': 5,
@@ -14,7 +11,6 @@ generalSettings = {
     'max_distance': None,
     'max_stops': 30,
     'max_runs': 1,
-    'exclude_groups': None,
     'avail_from': 900,
     'avail_till': 1700,
     'webhook_url': None,
