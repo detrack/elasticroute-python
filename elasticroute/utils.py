94ea8e0e9fcdf75612a88f4db999f25e77ce3d65
chesnutcase <chester8991@gmail.com>
2019-10-23 13:36:06 +0800

    delete old files

--- elasticroute/utils.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class EREncoder(json.JSONEncoder):
-    def default(self, o):
-        dict_function = getattr(o, "__dict__", None)
-        if callable(dict_function):
-            return o.__dict__()
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, o)
