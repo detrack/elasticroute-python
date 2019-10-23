220b49b0adfdd7c63ad63f688bdc2250c8cce22c
chesnutcase <chester8991@gmail.com>
2019-10-23 14:00:14 +0800

    add Plan object

--- elasticroute/routing.py
+++ elasticroute/routing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .common import Vehicle as BaseVehicle
 from .common import Stop as BaseStop
 from .common import Depot as BaseDepot
@@ -26,3 +28,41 @@ class Stop(BaseStop):
 class Depot(BaseDepot):
     # no difference from base depot for now
     pass
+
+
+class Plan():
+    generalSettings = {
+        "country": None,
+        "timezone": None,
+        "loading_time": None,
+        "buffer": None,
+        "service_time": None,
+        "depot_selection": None,
+        "depot_selection_radius": None,
+        "distance_unit": None,
+        "max_time": None,
+        "max_distance": None,
+        "max_stops": None,
+        "max_runs": None,
+        "from": None,
+        "till": None,
+        "webhook_url": None
+    }
+
+    def __init__(self, date=None, stops=None, depots=None, vehicles=None, rushHours=None, generalSettings=None):
+        self.date = date if date is not None else datetime.now().strftime("%Y-%m-%d")
+        self.stops = stops if stops is not None else []
+        self.depots = depots if depots is not None else []
+        self.vehicles = vehicles if vehicles is not None else []
+        self.rushHours = rushHours if rushHours is not None else []
+        self.generalSettings = generalSettings if generalSettings is not None else self.__class__().generalSettings
+
+    def __dict__(self):
+        return {
+            "date": self.date,
+            "stops": self.stops,
+            "depots": self.depots,
+            "vehicles": self.vehicles,
+            "rushHours": self.rushHours,
+            "generalSettings": self.generalSettings
+        }
