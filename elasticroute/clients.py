9ecede5032316942c3ebad1858c83f8bbd12ea47
chesnutcase <chester8991@gmail.com>
2019-10-23 13:21:25 +0800

    add vehicle repository to dashboardclient

--- elasticroute/clients.py
+++ elasticroute/clients.py
@@ -1,6 +1,6 @@
 from .repositories import StopRepository, VehicleRepository
-from .serializers import DashboardStopSerializer
-from .deserializers import DashboardStopDeserializer
+from .serializers import DashboardStopSerializer, DashboardVehicleSerializer
+from .deserializers import DashboardStopDeserializer, DashboardVehicleDeserializer
 from .validators import StopValidator, VehicleValidator
 
 
@@ -11,3 +11,4 @@ class DashboardClient():
     def __init__(self, api_key):
         self.api_key = api_key
         self.stops = StopRepository(serializer=DashboardStopSerializer(), client=self, deserializer=DashboardStopDeserializer(), validator=StopValidator())
+        self.vehicles = VehicleRepository(serializer=DashboardVehicleSerializer(), client=self, deserializer=DashboardVehicleDeserializer(), validator=VehicleValidator())
